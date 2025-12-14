ffa1b9cc8e6335e8ca2890ca5dbaa2bb1aaf4728
Makafui Lordd <kafuilord07@gmail.com>
2025-12-14 16:07:32 +0000

    Fixed bug that made the filter grid icons display on desktop view

--- src/components/shop/FilterBar.jsx
+++ src/components/shop/FilterBar.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./FilterBar.module.css";
 import Square from "../UI/FilterBoxes/Square";
 import DoubleRectangle from "../UI/FilterBoxes/DoubleRectangle";
 import FourSquare from "../UI/FilterBoxes/FourSquare";
+import { WindowSizeContext } from "../../store/windowSize-context";
 
 const FilterBar = ({ items, onFilterSelect, onGridChange }) => {
- 
+  const { isMobileSize } = useContext(WindowSizeContext);
 
   return (
     <div className={classes.filterBar}>
       <h4>shop</h4>
       <div className={classes.fo}>
         <h4>Filter and order</h4>
-        <div className={classes.filterGrid}>
-          <Square onClick={() => {onGridChange(1)}}/>
-          <DoubleRectangle onClick={() => {onGridChange(2)}}/>
-          <FourSquare onClick={() => {onGridChange(3)}}/>
-        </div>
+
+        {isMobileSize && (
+          <div className={classes.filterGrid}>
+            <Square
+              onClick={() => {
+                onGridChange(1);
+              }}
+            />
+            <DoubleRectangle
+              onClick={() => {
+                onGridChange(2);
+              }}
+            />
+            <FourSquare
+              onClick={() => {
+                onGridChange(3);
+              }}
+            />
+          </div>
+        )}
       </div>
       <ul className={classes.filter}>
         {items.map((item, index) => (
